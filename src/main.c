4f121cda1c8b4d210573a4693a44aee951d69e4e
Ster Van asselt <svan-ass@f1r6s16.codam.nl>
2022-08-03 13:35:49 +0200

    fixed

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: svan-ass <svan-ass@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/08/02 10:31:58 by rkoper        #+#    #+#                 */
-/*   Updated: 2022/08/03 13:31:06 by rkoper        ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svan-ass <svan-ass@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 10:31:58 by rkoper            #+#    #+#             */
+/*   Updated: 2022/08/03 13:35:02 by svan-ass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@ void	hook(void *param)
 
 int	main(void)
 {
-	t_data data;
-	
+	t_data	data;
+
 	data.mlx = mlx_init(1000, 1000, "MLX42", true);
 	if (!data.mlx)
 		exit(EXIT_FAILURE);
 	data.g_img = mlx_new_image(data.mlx, 128, 128);
-	memset(data.g_img->pixels, 250, data.g_img->width * data.g_img->height * sizeof(int) * 2);
+	memset(data.g_img->pixels, 255, data.g_img->width * data.g_img->height \
+	* sizeof(int));
 	mlx_image_to_window(data.mlx, data.g_img, 0, 0);
 	mlx_loop_hook(data.mlx, &hook, data.mlx);
 	mlx_loop(data.mlx);
