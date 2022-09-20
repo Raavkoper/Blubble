c70f6a98fdfd297af6057ea9db8e79c2da4c8c9d
Ster Van asselt <svan-ass@f1r6s16.codam.nl>
2022-09-20 13:52:34 +0200

    fixed WASD

--- src/key_input.c
+++ src/key_input.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   key_input.c                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: svan-ass <svan-ass@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/09/05 14:10:43 by svan-ass      #+#    #+#                 */
-/*   Updated: 2022/09/20 13:10:50 by rkoper        ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   key_input.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svan-ass <svan-ass@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 14:10:43 by svan-ass          #+#    #+#             */
+/*   Updated: 2022/09/20 13:52:20 by svan-ass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,42 @@
 
 void	input_data_up(t_data *data)
 {
-	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx + data->cam.dirx * 0.03)] == '0')
+	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx + \
+	data->cam.dirx * 0.03)] == '0')
 		data->cam.posx += data->cam.dirx * 0.03;
-	if (data->map.map[(int)(data->cam.posy + data->cam.diry * 0.03)][(int)data->cam.posx] == '0')
+	if (data->map.map[(int)(data->cam.posy + data->cam.diry * \
+	0.03)][(int)data->cam.posx] == '0')
 		data->cam.posy += data->cam.diry * 0.03;
 }
 
 void	input_data_down(t_data *data)
 {	
-	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx - data->cam.dirx * 0.03)] == '0')
+	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx - \
+	data->cam.dirx * 0.03)] == '0')
 		data->cam.posx -= data->cam.dirx * 0.03;
-	if (data->map.map[(int)(data->cam.posy - data->cam.diry * 0.03)][(int)data->cam.posx] == '0')
+	if (data->map.map[(int)(data->cam.posy - data->cam.diry * \
+	0.03)][(int)data->cam.posx] == '0')
 		data->cam.posy -= data->cam.diry * 0.03;
+}
+
+void	input_data_left(t_data *data)
+{
+	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx - \
+	data->cam.diry * 0.03)] == '0')
+		data->cam.posx -= data->cam.diry * 0.03;
+	if (data->map.map[(int)(data->cam.posy + data->cam.dirx * \
+	0.03)][(int)data->cam.posx] == '0')
+	data->cam.posy += data->cam.dirx * 0.03;
+}
+
+void	input_data_right(t_data *data)
+{
+	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx + \
+	data->cam.diry * 0.03)] == '0')
+		data->cam.posx += data->cam.diry * 0.03;
+	if (data->map.map[(int)(data->cam.posy - data->cam.dirx * \
+	0.03)][(int)data->cam.posx] == '0')
+		data->cam.posy -= data->cam.dirx * 0.03;
 }
 
 void	input_data_rotate_left(t_data *data)
@@ -76,10 +100,10 @@ void	key_input(t_data *data)
 		input_data_up(data);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_S))
 		input_data_down(data);
-	// if (mlx_is_key_down(data->mlx, MLX_KEY_A))
-	// 	input_data_left(data);
-	// if (mlx_is_key_down(data->mlx, MLX_KEY_D))
-	// 	input_data_right(data);
+	if (mlx_is_key_down(data->mlx, MLX_KEY_A))
+		input_data_left(data);
+	if (mlx_is_key_down(data->mlx, MLX_KEY_D))
+		input_data_right(data);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_LEFT))
 		input_data_rotate_left(data);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_RIGHT))
