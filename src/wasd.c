3d9b6a7d286f3d9ba36cb7415bd393cc731b7fee
Ster Van asselt <svan-ass@f1r6s18.codam.nl>
2022-10-10 11:39:24 +0200

    old textures back fishy

--- src/wasd.c
+++ src/wasd.c
@@ -6,7 +6,7 @@
 /*   By: svan-ass <svan-ass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 12:41:20 by svan-ass          #+#    #+#             */
-/*   Updated: 2022/10/06 12:42:15 by svan-ass         ###   ########.fr       */
+/*   Updated: 2022/10/10 11:36:54 by svan-ass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,39 @@
 void	input_data_up(t_data *data)
 {
 	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx + \
-	data->cam.dirx * 0.03)] == '0')
-		data->cam.posx += data->cam.dirx * 0.03;
+	data->cam.dirx * data->player.movespeed)] == '0')
+		data->cam.posx += data->cam.dirx * data->player.movespeed;
 	if (data->map.map[(int)(data->cam.posy + data->cam.diry * \
-	0.03)][(int)data->cam.posx] == '0')
-		data->cam.posy += data->cam.diry * 0.03;
+	data->player.movespeed)][(int)data->cam.posx] == '0')
+		data->cam.posy += data->cam.diry * data->player.movespeed;
 }
 
 void	input_data_down(t_data *data)
 {	
 	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx - \
-	data->cam.dirx * 0.03)] == '0')
-		data->cam.posx -= data->cam.dirx * 0.03;
+	data->cam.dirx * data->player.movespeed)] == '0')
+		data->cam.posx -= data->cam.dirx * data->player.movespeed;
 	if (data->map.map[(int)(data->cam.posy - data->cam.diry * \
-	0.03)][(int)data->cam.posx] == '0')
-		data->cam.posy -= data->cam.diry * 0.03;
+	data->player.movespeed)][(int)data->cam.posx] == '0')
+		data->cam.posy -= data->cam.diry * data->player.movespeed;
 }
 
 void	input_data_left(t_data *data)
 {
 	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx - \
-	data->cam.diry * 0.03)] == '0')
-		data->cam.posx -= data->cam.diry * 0.03;
+	data->cam.diry * data->player.movespeed)] == '0')
+		data->cam.posx -= data->cam.diry * data->player.movespeed;
 	if (data->map.map[(int)(data->cam.posy + data->cam.dirx * \
-	0.03)][(int)data->cam.posx] == '0')
-	data->cam.posy += data->cam.dirx * 0.03;
+	data->player.movespeed)][(int)data->cam.posx] == '0')
+	data->cam.posy += data->cam.dirx * data->player.movespeed;
 }
 
 void	input_data_right(t_data *data)
 {
 	if (data->map.map[(int)data->cam.posy][(int)(data->cam.posx + \
-	data->cam.diry * 0.03)] == '0')
-		data->cam.posx += data->cam.diry * 0.03;
+	data->cam.diry * data->player.movespeed)] == '0')
+		data->cam.posx += data->cam.diry * data->player.movespeed;
 	if (data->map.map[(int)(data->cam.posy - data->cam.dirx * \
-	0.03)][(int)data->cam.posx] == '0')
-		data->cam.posy -= data->cam.dirx * 0.03;
+	data->player.movespeed)][(int)data->cam.posx] == '0')
+		data->cam.posy -= data->cam.dirx * data->player.movespeed;
 }
