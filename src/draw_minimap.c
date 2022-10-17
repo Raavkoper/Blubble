c72d0077d791134a40c37aa8df48c1495e2267f6
Raav Koper <rkoper@f1r5s18.codam.nl>
2022-10-17 13:27:19 +0200

    fimnished minimap

--- src/draw_minimap.c
+++ src/draw_minimap.c
@@ -6,7 +6,7 @@
 /*   By: rkoper <rkoper@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 10:45:59 by rkoper        #+#    #+#                 */
-/*   Updated: 2022/10/16 18:30:28 by rkoper        ########   odam.nl         */
+/*   Updated: 2022/10/17 13:26:29 by rkoper        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,15 @@ void	set_map_to_zero(t_data *data)
 	int y;
 
 	y = 0;
-	while (y < 14)
+	while (y < 15)
 	{
 		x = 0;
-		while (x < 14)
+		while (x < 15)
 		{
 			data->minimap.map[y][x] = ' ';
 			x++;
 		}
 		y++;
-	}
-}
-
-void	print_map(char map[15][15], t_data *data)
-{
-	int x = 0;
-	int y = 0;
-
-	while (y < 14)
-	{
-		x = 0;
-		while (x < 14)
-		{
-			if (y == 7 && x == 7)
-				printf("P");
-			else
-			{
-				printf("%c", map[y][x]);
-			}
-			x++;
-		}	
-		y++;
-		printf("\n");
 	}
 }
 
@@ -74,7 +51,9 @@ void	update_minimap(t_data *data)
 			if ((int)data->cam.posy + y2 > data->map.height || (int)data->cam.posx + x2 > data->map.width \
 				|| (int)data->cam.posy + y2 < 0)	
 				break ;
-			if ((int)data->cam.posx + x2 >= 0 && map[(int)data->cam.posy + y2][(int)data->cam.posx + x2] != '\n')
+			if (x == 0 || x == 14 || y == 0)
+				data->minimap.map[y][x] = ' ';
+			else if ((int)data->cam.posx + x2 >= 0 && map[(int)data->cam.posy + y2][(int)data->cam.posx + x2] != '\n')
 				data->minimap.map[y][x] = map[(int)data->cam.posy + y2][(int)data->cam.posx + x2];
 			x2++;
 			x++;
@@ -82,8 +61,6 @@ void	update_minimap(t_data *data)
 		y2++;
 		y++;
 	}
-	print_map(data->minimap.map, data);
-	printf("\n\n\n");
 }
 
 void	draw_minimap(t_data *data)
@@ -96,12 +73,14 @@ void	draw_minimap(t_data *data)
 	int y2;
 	int empty;
 	int player;
+	int walls;
 	int color;
 	int darky;
 	
 	player = create_rgba(255, 105, 180, 255);
 	empty = create_rgba(216, 191, 216, 255);
 	darky = create_rgba(150, 150, 150, 255);
+	walls = create_rgba(38, 38, 255, 255);
 	y = data->minimap.y_start;
 	y2 = 0;
 	while (y < data->minimap.y_end)
@@ -117,9 +96,9 @@ void	draw_minimap(t_data *data)
 				if (y2 == 7 && x2 == 7)
 					color = player;
 				else if (data->minimap.map[y2][x2] == '0')
-					color = empty;
+					color = data->f_color;
 				else if (data->minimap.map[y2][x2] == '1')
-					color = 0;
+					color = walls;
 				else
 					color = darky;
 				while (x < xhold + (data->minimap.width / 15))
@@ -127,7 +106,6 @@ void	draw_minimap(t_data *data)
 					mlx_put_pixel(data->g_img, x, y, color);
 					x++;
 				}
-				// printf("y row: %d, x row: %d, color: %d\n", y2, x2, color);
 				x2++;
 			}
 			y++;
